cc171f7b9b9c6af992d2a682100263b5ec1e3d37
muhammedjavadksd <javadjazz899@gmail>
2024-09-21 12:51:37 +0530

    admin start

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const app = express();
 const dotenv = require("dotenv")
 const proxy = require("express-http-proxy")
 const cors = require("cors")
+const logger = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+app.use(logger("combined"))
 
 app.use(cors({
-    origin: ['http://localhost:3000']
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:7001',
+    ]
 }))
 //Config
 dotenv.config("./.env");
@@ -19,6 +26,7 @@ const PORT = process.env.PORT || 7001
 const AUTH_ENDPOINT = process.env.AUTH_ENDPOINT
 const PROFILE_ENDPOINT = process.env.PROFILE_ENDPOINT
 const FUND_RAISE_EDNPOINT = process.env.FUND_RAISE_EDNPOINT
+const BLOOD_ENDPOINT = process.env.BLOOD_ENDPOINT
 
 // app.use(express.json({ limit: "4mb" }))
 // app.use(express.urlencoded({ limit: "4mb" }))
@@ -31,6 +39,7 @@ console.log("Profile service endpoint", PROFILE_ENDPOINT);
 app.use("/api/auth", proxy(AUTH_ENDPOINT))
 app.use("/api/profile", proxy(PROFILE_ENDPOINT))
 app.use("/api/fund_raise", proxy(FUND_RAISE_EDNPOINT))
+app.use("/api/blood", proxy(BLOOD_ENDPOINT))
 
 
 
